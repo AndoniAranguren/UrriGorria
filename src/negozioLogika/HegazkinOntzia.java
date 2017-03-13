b50a67d16724ce1d506c3aee0ee3f91ebe77e7a6
jonirazabal <jonirazabal14@gmail.com>
2017-03-14 00:12:50 +0100

    Update HegazkinOntzia.java

--- src/negozioLogika/HegazkinOntzia.java
+++ src/negozioLogika/HegazkinOntzia.java
@@ -1,5 +1,17 @@
 package negozioLogika;
 
-public class HegazkinOntzia {
+public class HegazkinOntzia extends Itsasontzia {
+
+	public HegazkinOntzia() {
+		super(4, 600);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void informazioaInprimatu() {
+		// TODO Auto-generated method stub
+		System.out.println("       Luzera: "+super.getLuzera());
+		System.out.println("       Prezioa: "+super.getPrezioa());
+
+	}
 
 }
