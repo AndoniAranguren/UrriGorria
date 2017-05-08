cc0749848617bb1f7dc4e02a3ac4697602b2b100
Kaskagues <andoni.aranguren@gmail.com>
2017-05-08 19:45:22 +0200

    CPU

---
+++ src/negozioLogika/CPU.java
@@ -0,0 +1,10 @@
+package negozioLogika;
+
+public class CPU extends Jokalariak {
+	int zailtasuna;
+	public CPU(String pIzena, int pZailtasuna) {
+		super(pIzena);
+		zailtasuna=pZailtasuna;
+	}
+
+}
